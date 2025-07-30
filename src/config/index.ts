43d65250bc9c21b6e4b4d9b18f42fab99c284ba7
Imran Faridi <imran.faridi@e14s.com>
2025-07-30 09:51:29 -0400

    updated

--- src/config/index.ts
+++ src/config/index.ts
@@ -7,7 +7,7 @@ export const config: {
   bucket_name: string,
   aws_region: string 
 } = {
-  bucket_name: process.env.BUCKET_NAME ?? 'fhir-bulk-data-1',
+  bucket_name: process.env.BUCKET_NAME ?? 'fhir-bulk-data',
   aws_region: process.env.AWS_REGION ?? 'us-east-1'
 }
 
